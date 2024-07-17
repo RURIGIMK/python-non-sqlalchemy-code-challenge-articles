00f504ca688cd69cf831c3d1b6e0e1c5a9bc8c50
RURIGIMK <rurigimk@gmail.com>
2024-07-17 18:19:13 +0300

    Rurigi

--- lib/debug.py
+++ lib/debug.py
@@ -8,6 +8,23 @@ from classes.many_to_many import Magazine
 if __name__ == '__main__':
     print("HELLO! :) let's debug :vibing_potato:")
 
+    # Create some authors
+    author1 = Author("RURIGI")
+    author2 = Author("MAINA")
+
+    # Create some magazines
+    magazine1 = Magazine("FEELING TECHY?", "Technology")
+    magazine2 = Magazine("WHAT'S YOUR BODY SAYING?", "Health")
+
+    # Create some articles
+    article1 = Article(author1, magazine1, "The Future of AI")
+    article2 = Article(author1, magazine2, "Healthy Living Tips")
+    article3 = Article(author2, magazine1, "Quantum Computing in today's world")
+
+    # Add articles to authors
+    author1.add_article(magazine1, "The Future of AI")
+    author1.add_article(magazine2, "Healthy Living Tips")
+    author2.add_article(magazine1, "Quantum Computing in today's world")
 
     # don't remove this line, it's for debugging!
     ipdb.set_trace()
