fc13295aeeeaa1096e101cf8782942b2b9423d99
RURIGIMK <rurigimk@gmail.com>
2024-07-17 18:52:31 +0300

    RURIGI

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,3 +1,21 @@
+# many_to_many.py
+
+class Article:
+    def __init__(self, author, magazine, title):
+        self._title = title
+        self.author = author
+        self.magazine = magazine
+        self.magazine.add_article(self)
+        self.author.add_article(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, new_title):
+        raise AttributeError("Title attribute is immutable.")
+
 class Author:
     def __init__(self, name):
         self._name = name
@@ -7,22 +25,11 @@ class Author:
     def name(self):
         return self._name
 
-    @property
+    def add_article(self, article):
+        self._articles.append(article)
+
     def articles(self):
         return self._articles
-
-    def add_article(self, magazine, title):
-        article = Article(self, magazine, title)
-        self._articles.append(article)
-        magazine.add_article(article)
-        return article
-
-    @property
-    def magazines(self):
-        return list(set(article.magazine for article in self.articles))
-
-    def topic_areas(self):
-        return list(set(article.magazine.category for article in self.articles))
 
 class Magazine:
     def __init__(self, name, category):
@@ -50,166 +57,29 @@ class Magazine:
             raise ValueError("Category must be a non-empty string.")
         self._category = new_category
 
-    @property
-    def articles(self):
-        return self._articles
-
     def add_article(self, article):
         self._articles.append(article)
 
-    def article_titles(self):
-        if not self.articles:
-            return None
-        return [article.title for article in self.articles]
-
-    def contributing_authors(self):
-        author_counts = {}
-        for article in self.articles:
-            author = article.author
-            if author in author_counts:
-                author_counts[author] += 1
-            else:
-                author_counts[author] = 1
-        return [author for author, count in author_counts.items() if count > 2]
-
-class Article:
-    def __init__(self, author, magazine, title):
-        self._author = author
-        self._magazine = magazine
-        self._title = title
-
-    @property
-    def author(self):
-        return self._author
-
-    @author.setter
-    def author(self, new_author):
-        self._author = new_author
-
-    @property
-    def magazine(self):
-        return self._magazine
-
-    @magazine.setter
-    def magazine(self, new_magazine):
-        self._magazine = new_magazine
-
-    @property
-    def title(self):
-        return self._title
-
-    @property
-    def magazine(self):
-        return self._magazine
-
-    @title.setter
-    def title(self, new_title):
-        raise AttributeError("Title attribute is immutable.")
-
-class Author:
-    def __init__(self, name):
-        self._name = name
-        self._articles = []
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
     def articles(self):
         return self._articles
 
-    def add_article(self, magazine, title):
-        article = Article(self, magazine, title)
-        self._articles.append(article)
-        magazine.add_article(article)
-        return article
-
-    @property
-    def magazines(self):
-        return list(set(article.magazine for article in self.articles))
-
-    def topic_areas(self):
-        return list(set(article.magazine.category for article in self.articles))
-
-class Magazine:
-    def __init__(self, name, category):
-        self._name = name
-        self._category = category
-        self._articles = []
-
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, new_name):
-        if not isinstance(new_name, str) or not (2 <= len(new_name) <= 16):
-            raise ValueError("Name must be a string between 2 and 16 characters.")
-        self._name = new_name
-
-    @property
-    def category(self):
-        return self._category
-
-    @category.setter
-    def category(self, new_category):
-        if not isinstance(new_category, str) or len(new_category) == 0:
-            raise ValueError("Category must be a non-empty string.")
-        self._category = new_category
-
-    @property
-    def articles(self):
-        return self._articles
-
-    def add_article(self, article):
-        self._articles.append(article)
+    def contributors(self):
+        contributors = set()
+        for article in self._articles:
+            contributors.add(article.author)
+        return list(contributors)
 
     def article_titles(self):
-        if not self.articles:
-            return None
-        return [article.title for article in self.articles]
+        return [article.title for article in self._articles]
 
     def contributing_authors(self):
-        author_counts = {}
-        for article in self.articles:
-            author = article.author
-            if author in author_counts:
-                author_counts[author] += 1
+        authors_dict = {}
+        for article in self._articles:
+            if article.author in authors_dict:
+                authors_dict[article.author] += 1
             else:
-                author_counts[author] = 1
-        return [author for author, count in author_counts.items() if count > 2]
+                authors_dict[article.author] = 1
+        return [author for author, count in authors_dict.items() if count > 2]
 
-class Article:
-    def __init__(self, author, magazine, title):
-        self._author = author
-        self._magazine = magazine
-        self._title = title
-
-    @property
-    def author(self):
-        return self._author
-
-    @author.setter
-    def author(self, new_author):
-        self._author = new_author
-
-    @property
-    def magazine(self):
-        return self._magazine
-
-    @magazine.setter
-    def magazine(self, new_magazine):
-        self._magazine = new_magazine
-
-    @property
-    def title(self):
-        return self._title
-
-    @property
-    def magazine(self):
-        return self._magazine
-
-    @title.setter
-    def title(self, new_title):
-        raise AttributeError("Title attribute is immutable.")
+    def __str__(self):
+        return f"{self._name} ({self._category})"
